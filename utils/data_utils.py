e725f1a242e80ed7af5293bef4c2890453752c67
Akib Mohammed Khan <akibmohammed@iut-dhaka.edu>
2022-08-21 18:29:32 +0600

    augmented data directory added

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -23,11 +23,6 @@ def get_loader(args):
         transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-        transforms.RandomHorizontalFlip(p=0.2),
-        transforms.RandomApply([transforms.RandomChoice([transforms.RandomAffine(degrees=20, translate=(0.1,0.3), scale=(0.05, 0.75)),transforms.RandomRotation(50)])],p=0.2),
-        transforms.RandomApply([transforms.RandomAdjustSharpness(sharpness_factor=2)],p=0.1),
-        transforms.RandomApply([transforms.GaussianBlur(kernel_size=(3, 5), sigma=(0.1, 5))],p=0.1),
-        transforms.RandomApply([transforms.RandomChoice([transforms.RandomAutocontrast(),transforms.ColorJitter(brightness=0.5, hue=0.3)])],p=0.2)
     ])
 
     transform_test = transforms.Compose([
@@ -48,14 +43,14 @@ def get_loader(args):
 
     else:
         if args.augmentation == True:
-            trainset = datasets.ImageFolder(root="./data/DatasetV2/train",
+            trainset = datasets.ImageFolder(root="./data/augmented_dataset_v1/train",
                                      transform=transform_train_aug)
         else :
-            trainset = datasets.ImageFolder(root="./data/DatasetV2/train",
+            trainset = datasets.ImageFolder(root="./data/augmented_dataset_v1/train",
                                      transform=transform_train)
-        validset = datasets.ImageFolder(root="./data/DatasetV2/valid",
+        validset = datasets.ImageFolder(root="./data/augmented_dataset_v1/valid",
                                     transform=transform_test) if args.local_rank in [-1, 0] else None
-        testset = datasets.ImageFolder(root="./data/DatasetV2/test",
+        testset = datasets.ImageFolder(root="./data/augmented_dataset_v1/test",
                                     transform=transform_test) if args.local_rank in [-1, 0] else None
     if args.local_rank == 0:
         torch.distributed.barrier()
